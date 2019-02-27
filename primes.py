2e325d0cc5d27690d72979db7dfabb1801a9cf8e
JesusHilario <33611574+JesusHilario@users.noreply.github.com>
2019-02-26 19:35:56 -0600

    Examen

--- primes.py
+++ primes.py
@@ -4,13 +4,34 @@ Your module documentation here
 
 
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
 
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+    def aplicacion(self):
+        # Agregamos una excepcion en caso de ingresar un valor no numero
+        try:
+            # Cambiamos a entero
+            numero = int(input('Ingresa un numero: '))
+    # Evaluamos si es primo con la funcion
+            result = self.esPrimo(numero)
+    # Evaluamos la respuesta
+            if result is True:
+                print('El numero es primo!!')
+                self.aplicacion()
+            else:
+                print('El numero no es primo!')
+                self.aplicacion()
+        except ValueError:
+            print("No es un numero!")
+            self.aplicacion()
+
+    def esPrimo(self, numero):
+        # Validamos los numeros que son primos y pueden dar residuo de 1
+        if numero < 1:
+            return False
+        elif numero == 2:
+            return True
+        else:
+            # Cualquier numero que tenga mas de dos divisores no es primo
+            for i in range(2, numero):
+                if numero % i == 0:
+                    return False
+        return True
